0f22a24da7a89dbea4e453be4a70e8b01d6eef5a
gautur16 <gautur16@ru.is>
2017-05-11 22:21:52 +0000

    Fixed fileType and syntax

--- DoriVLN/DoriVLN/Models/ViewModels/EditorViewModel.cs
+++ DoriVLN/DoriVLN/Models/ViewModels/EditorViewModel.cs
@@ -9,5 +9,6 @@ namespace DoriVLN.Models.ViewModels
     {
         public string Content { get; set; }
         public string fileName { get; set; }
+        public string fileType { get; set; }
     }
 }
